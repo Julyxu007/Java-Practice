aedf207788de94eb80c8818dd20c95a6cd853bf8
julyxu <julyxu0720@gmail.com>
2026-03-05 10:35:30 +1300

    addnew

---
+++ src/Basic/StringBasics.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class StringBasics {
+
+    public static void main (String[] args){
+
+        String message = "\\\"learning java  \"";
+        System.out.println(message.toUpperCase());
+        System.out.println(message.trim());
+        String message2 = "Good \nMorning";
+        System.out.println(message2);
+        String message3 = "Hi:\t Winter";
+        System.out.println(message3);
+    }
+}
