ebd17d39aef842812e1a97042b0a4fb6b56eb8db
julyxu <julyxu0720@gmail.com>
2026-03-10 22:10:02 +1300

    forloop

--- src/Basic/ControlFlow/Loops.java
+++ src/Basic/ControlFlow/Loops.java
@@ -1,4 +1,13 @@
 package Basic.ControlFlow;
 
 public class Loops {
+    public static void main(String[] args) {
+        //for loop
+        for (int i = 0; i < 5; i++){
+            System.out.println("----");
+        }
+        for (int i = 5; i >0; i--){
+            System.out.println("/// " + i);
+        }
+    }
 }
