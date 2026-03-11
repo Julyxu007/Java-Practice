22365ab2a4dbe1ce3f9b0d1248dad1e41adcf29e
julyxu <julyxu0720@gmail.com>
2026-03-11 21:53:16 +1300

    enhance MortgageProject

--- src/Basic/ControlFlow/Loops.java
+++ src/Basic/ControlFlow/Loops.java
@@ -1,5 +1,7 @@
 package Basic.ControlFlow;
 
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
         //for loop
@@ -9,5 +11,53 @@ public class Loops {
         for (int i = 5; i >0; i--){
             System.out.println("/// " + i);
         }
+
+
+// while loop
+        int i = 5;
+        while (i > 0){
+            System.out.println("=== " + i);
+            i--;
+        }
+
+
+
+
+        // not sure how many times
+        String input = "";
+        // better to put scanner outside of the loop
+        Scanner scanner = new Scanner(System.in);
+        // while true；make sure have a break statement;
+        while (true){
+            System.out.print("Input: ");
+           input = scanner.next().toLowerCase();
+           if (input.equals("pass")){
+               continue;
+           }
+           if (input.equals("quit")){
+               break;
+           }
+            System.out.println(input);
+
+        }
+
+    // do while (even if the condition is false, do while loop will at least get executed once)
+        do {System.out.println("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);} while(!input.equals("quit"));
+
+        // break and continue
+      // break terminate the loop; continue moves to the beginning of the loop
+
+     //for each loop
+
+        String[] fruits = {"apple", "pear", "kiwi"};
+        for (int j = 0; j < fruits.length; j++) {
+            System.out.println(fruits[j]);
+        }
+        for (String item: fruits){
+            System.out.println(item);
+        }
+
     }
 }
--- src/Basic/MortgageProject.java
+++ src/Basic/MortgageProject.java
@@ -7,19 +7,42 @@ import java.util.Scanner;
 public class MortgageProject {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principle: ");
-        int principle = Integer.parseInt(scanner.nextLine());
+        int principle;
+        double interestRate;
+        int years;
+        while (true) {
+            System.out.print("Principle ($1K - $1M): ");
+            principle = Integer.parseInt(scanner.nextLine());
+            if ( 1000 <= principle && principle <= 1000000) {
+                break;
+            }
+            System.out.println("Enter a number between 1000 and 1000000.");
+        }
+         while (true) {
+             System.out.print("Annual Interest Rate: ");
+             interestRate = scanner.nextDouble();
+             if (0 < interestRate && interestRate < 30 ) {
+                 break;
+             }
+             System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+         }
 
-        System.out.print("Annual Interest Rate: ");
-        double interestRate = scanner.nextDouble();
-        double r = interestRate / 100 /12;
+            double r = interestRate / 100 /12;
 
-        System.out.print("Period (Years): ");
-        int years = scanner.nextInt();
 
-        double mathPow = Math.pow(1+r, years *12);
-        double mortgage = principle * ((r * mathPow) / (mathPow -1));
-        String mortgageCurrency = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageCurrency);
+         while (true) {
+             System.out.print("Period (Years): ");
+             years = scanner.nextInt();
+             if ( 1 <= years && years <=  30){
+                 break;
+             }
+             System.out.println("Enter a value between 1 and 30");
+         }
+
+            double mathPow = Math.pow(1+r, years *12);
+            double mortgage = principle * ((r * mathPow) / (mathPow -1));
+            String mortgageCurrency = NumberFormat.getCurrencyInstance().format(mortgage);
+            System.out.println("Mortgage: " + mortgageCurrency);
+        }
+
     }
-}
